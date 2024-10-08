456c35c3cd3380bd160a6da885027d9cb18fe709
Jerry291299 <ungductrungtrung@gmail.com>
2024-10-08 21:16:13 +0700

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './layout/Home';
 function App() {
   return (
     <>
     <BrowserRouter>
     <Routes>
-      {/* <Route path="/" element={<Home />} /> */}
-
-
-
-
-
-
-
-
-
+      <Route path="/" element={<Home />} />
 
 
 
