65fa29f429a4725a6dc26225df54a95867c61346
Jerry291299 <ungductrungtrung@gmail.com>
2024-10-16 02:17:39 +0700

    ssss

--- src/components/user/register.tsx
+++ src/components/user/register.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from '../img/Black & White Minimalist Aesthetic Initials Font Logo.png'
+import { NavLink } from 'react-router-dom'
 const Register = () => {
   return (
     <section className="bg-gray-300 ">
@@ -28,8 +29,8 @@ const Register = () => {
                     </div>
                     
                     <button type="submit" className="w-full text-white bg-blue-600 hover:bg-orange-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>              
-                          <p className="text-sm font-light text-gray-500 text-gray-400">
-                        Already have an account? <a href="#" className="font-medium text-primary-600 hover:underline text-primary-500">Login here</a>
+                          <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                        Already have an account? <NavLink to={"/login"} className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login</NavLink>
                     </p>
                 </form>
             </div>
@@ -42,3 +43,4 @@ const Register = () => {
 
 
 export default Register
+
