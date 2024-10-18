6fc3424b4ee19a37c5a4fe18eabd2b35f703cedd
Jerry291299 <ungductrungtrung@gmail.com>
2024-10-18 15:43:09 +0700

    rework lai login + header

--- src/service/user.tsx
+++ src/service/user.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { axiosservice } from '../config/API'
+import { IUserLogin, IUserRegister } from '../interface/user'
 
 
 export const getAllusers = async () => {
@@ -8,5 +9,26 @@ export const getAllusers = async () => {
     return data    
   } catch (error) {
     console.log(error);
+  }
+}
+
+export const UserLogin = async (datauser : IUserLogin) => {
+  try {
+      const {data} = await axiosservice.post('/login', datauser)
+      return data
+  } catch (error) {
+      console.error('Login error:', error);
+      throw error;
+      
+  }
+}
+
+export const UserRegister = async (datauser : IUserRegister) => {
+  try {
+      const {data} = await axiosservice.post('/register', datauser)
+      return data
+  } catch (error) {
+      console.error('Register error:', error);
+      throw error;
   }
 }
