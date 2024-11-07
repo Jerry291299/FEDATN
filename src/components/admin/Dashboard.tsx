989f9fdc0a4209aee4731313110436a103b6ceb0
ngochai18 <ngochai180605@gmail.com>
2024-11-08 02:42:41 +0700

    active deactive product

--- src/components/admin/Dashboard.tsx
+++ src/components/admin/Dashboard.tsx
@@ -1,20 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { NavLink, useNavigate, useParams } from 'react-router-dom';
-import { DeleteProduct, getAllproducts } from '../../service/products';
-import { Iproduct } from '../../interface/products';
-import { Icategory } from '../../interface/category';
-import { Popconfirm } from 'antd';
-import LoadingComponent from '../Loading';
+import React, { useEffect, useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import {
+  DeactivateProduct,
+  ActivateProduct,
+  getAllproducts,
+} from "../../service/products";
+import { Iproduct } from "../../interface/products";
+import { Popconfirm } from "antd";
+import LoadingComponent from "../Loading";
 
 type Props = {};
 
 const Dashboard = (props: Props) => {
   const [products, setProduct] = useState<Iproduct[]>([]);
-  const [filterName, setFilterName] = useState<string>(''); // State để lưu giá trị lọc theo tên
-  const param = useParams();
+  const [filterName, setFilterName] = useState<string>(""); // State để lưu giá trị lọc theo tên
   const navigate = useNavigate();
   const [loading, setLoading] = useState<boolean>(false);
-  
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -31,27 +33,49 @@ const Dashboard = (props: Props) => {
     fetchData();
   }, []);
 
-  const delProduct = async (id: string) => {
-    await DeleteProduct(id);
-    const newproduct = products.filter((product: Iproduct) => product._id !== id);
-    console.log(id);
-    setProduct(newproduct);
-  };
-
   const updateProduct = (id: string) => {
     navigate(`update/${id}`);
   };
 
+  // Hàm cập nhật trạng thái sản phẩm
+  const toggleProductStatus = async (id: string, status: boolean) => {
+    try {
+      // Kiểm tra trạng thái hiện tại của sản phẩm và gọi API thích hợp
+      if (status) {
+        await DeactivateProduct(id); // Gọi API để vô hiệu hóa sản phẩm
+      } else {
+        await ActivateProduct(id); // Gọi API để kích hoạt sản phẩm
+      }
+
+      // Cập nhật lại trạng thái của sản phẩm trong danh sách
+      const updatedProducts = products.map((product) =>
+        product._id === id
+          ? {
+            ...product,
+            status: !status, // Đảo ngược trạng thái boolean: từ "active" sang "deactive" và ngược lại
+          }
+          : product
+      );
+
+      setProduct(updatedProducts); // Cập nhật lại danh sách sản phẩm sau khi thay đổi trạng thái
+    } catch (error) {
+      console.error("Error toggling product status:", error);
+      // Xử lý lỗi nếu có
+    }
+  };
+
   // Lọc danh sách sản phẩm dựa trên tên sản phẩm
-  const filteredProducts = products.filter(product =>
+  const filteredProducts = products.filter((product) =>
     product.name.toLowerCase().includes(filterName.toLowerCase())
   );
 
   return (
     <>
       {loading && <LoadingComponent />}
-      <NavLink to={'/admin/add'}>
-        <button className=' focus:outline-none text-white bg-sky-600 hover:bg-sky-900 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2'>Thêm mới</button>
+      <NavLink to={"/admin/add"}>
+        <button className=" focus:outline-none text-white bg-sky-600 hover:bg-sky-900 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
+          Thêm mới
+        </button>
       </NavLink>
 
       {/* Trường nhập để lọc sản phẩm theo tên */}
@@ -70,53 +94,142 @@ const Dashboard = (props: Props) => {
               <table className="min-w-full table-auto">
                 <thead className="bg-white border-b">
                   <tr>
-                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">Stt</th>
-                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">Tên Sản Phẩm</th>
-                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">Giá</th>
-                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">Danh mục</th>
-                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">Số lượng</th>
-                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">Mô tả</th>
-                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">Ảnh</th>
-                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">Handle</th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                    >
+                      Stt
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                    >
+                      Tên Sản Phẩm
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                    >
+                      Giá
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                    >
+                      Danh mục
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                    >
+                      Số lượng
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                    >
+                      Mô tả
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                    >
+                      Ảnh
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                    >
+                      Trạng Thái
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                    >
+                      Handle
+                    </th>
                   </tr>
                 </thead>
                 <tbody>
-                  {filteredProducts.map((product: Iproduct, index: number) => (
-                    <tr className="bg-gray-100 border-b" key={product._id}>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{index + 1}</td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{product.name}</td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{product.price}</td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{product?.category?.name}</td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{product.soLuong}</td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{product.moTa}</td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        <img className='w-[100px]' src={product?.img} alt="" />
-                      </td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        <button
-                          onClick={() => updateProduct(product._id)}
-                          type="button"
-                          className="focus:outline-none text-white bg-sky-600 hover:bg-sky-900 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-                        >
-                          Edit
-                        </button>
-                        <Popconfirm
-                          title="Delete the task"
-                          description="Are you sure to delete this task?"
-                          onConfirm={() => { delProduct(product._id); }}
-                          okText="Yes"
-                          cancelText="No"
-                        >
+                  {filteredProducts.map(
+                    (product: Iproduct, index: number) => (
+                      <tr
+                        className="bg-gray-100 border-b"
+                        key={product._id}
+                      >
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                          {index + 1}
+                        </td>
+                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                          {product.name}
+                        </td>
+                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                          {product.price}
+                        </td>
+                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                          {product?.category?.name}
+                        </td>
+                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                          {product.soLuong}
+                        </td>
+                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                          {product.moTa}
+                        </td>
+                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                          <img
+                            className="w-[100px]"
+                            src={product?.img}
+                            alt=""
+                          />
+                        </td>
+                        <td className={`text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap ${product.status === true ? "text-green-700" : 'text-red-700'}`}>
+                          {product.status === true
+                            ? "Hoạt động"
+                            : "Vô hiệu hóa"}
+                        </td>
+                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                           <button
+                            onClick={() =>
+                              updateProduct(
+                                product._id
+                              )
+                            }
                             type="button"
-                            className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                            className="focus:outline-none text-white bg-sky-600 hover:bg-sky-900 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
                           >
-                            Delete
+                            Edit
                           </button>
-                        </Popconfirm>
-                      </td>
-                    </tr>
-                  ))}
+
+                          {/* Nút Active/Deactive */}
+                          <Popconfirm
+                            title="Are you sure?"
+                            onConfirm={() =>
+                              toggleProductStatus(
+                                product._id,
+                                product.status
+                              )
+                            }
+                            okText="Yes"
+                            cancelText="No"
+                          >
+                            <button
+                              type="button"
+                              className={`focus:outline-none text-white ${product.status ===
+                                  true
+                                  ? "bg-red-700 hover:bg-red-800"
+                                  : "bg-green-700 hover:bg-green-800"
+                                } focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2`}
+                            >
+                              {product.status ===
+                                true
+                                ? "Deactive"
+                                : "Active"}
+                            </button>
+                          </Popconfirm>
+                        </td>
+                      </tr>
+                    )
+                  )}
                 </tbody>
               </table>
             </div>
--- src/interface/products.tsx
+++ src/interface/products.tsx
@@ -1,14 +1,16 @@
 import { Icategory } from "./category";
 
-
-
-export interface Iproduct{
-    _id: string,
-    name: string,
-    img: string,
-    price: number,
-    soLuong: number,
-    moTa: string,
-    category: Icategory,
+export interface Iproduct {
+    _id: string;
+    name: string;
+    img: string;
+    price: number;
+    soLuong: number;
+    moTa: string;
+    category: Icategory;
+    status: boolean;
 }
-export type IProductLite = Pick<Iproduct,'_id'|'name'| 'img' | 'price'|'category' >
+export type IProductLite = Pick<
+    Iproduct,
+    "_id" | "name" | "img" | "price" | "category" | "status"
+>;
