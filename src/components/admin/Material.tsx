921e3fbdc74966a7f4607ed933075da7c5c3b414
TuyenPH22936 <97202848+TuyenPH22936@users.noreply.github.com>
2024-11-27 13:49:39 +0700

    add xuất file

--- src/components/admin/Material.tsx
+++ src/components/admin/Material.tsx
@@ -96,7 +96,7 @@ const ListMaterial = (props: Props) => {
         className="focus:outline-none text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
         target="_blank"
       >
-        Xuất Chất liệu
+        Xuất file chất liệu
       </CSVLink>
 
       {/* Ô tìm kiếm vật liệu */}
--- src/components/admin/listcategory.tsx
+++ src/components/admin/listcategory.tsx
@@ -83,7 +83,7 @@ const Listcategory = (props: Props) => {
         className='focus:outline-none text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2'
         target="_blank"
       >
-        Xuất danh mục
+        Xuất file danh mục
       </CSVLink>
 
       {/* Search input */}
