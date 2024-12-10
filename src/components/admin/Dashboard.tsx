cd7598334d00a1dffc922ded06c0481c7cf2a626
Jerry291299 <ungductrungtrung@gmail.com>
2024-12-10 15:13:58 +0700

    fix giao dien

--- src/components/admin/Dashboard.tsx
+++ src/components/admin/Dashboard.tsx
@@ -15,12 +15,12 @@ type Props = {};
 const Dashboard = (props: Props) => {
   const [products, setProduct] = useState<Iproduct[]>([]);
   const [selectedCategory, setSelectedCategory] = useState<string>("");
-  const [filterMaterial, setFilterMaterial] = useState<string>(""); // State để lọc theo chất liệu
+  const [filterMaterial, setFilterMaterial] = useState<string>("");
   const [filterPrice, setFilterPrice] = useState<[number, number]>([
     0, 100000000,
-  ]); // State để lọc theo giá (ví dụ giá từ 0 đến 100000)
+  ]);
 
-  const [filterName, setFilterName] = useState<string>(""); // State để lưu giá trị lọc theo tên
+  const [filterName, setFilterName] = useState<string>("");
   const navigate = useNavigate();
   const [loading, setLoading] = useState<boolean>(false);
   const [pageConfig, setPageConfig] = useState<any>();
@@ -32,8 +32,8 @@ const Dashboard = (props: Props) => {
     return name.trim() === ""
       ? products
       : products.filter((product) =>
-        product.name.toLowerCase().includes(name.toLowerCase())
-      );
+          product.name.toLowerCase().includes(name.toLowerCase())
+        );
   };
   const fetchData = async (currentPage: number) => {
     try {
@@ -60,7 +60,6 @@ const Dashboard = (props: Props) => {
     navigate(`update/${id}`);
   };
 
-  // Xử lý khi người dùng chuyển trang
   const handlePageChange = (currentPage: number) => {
     setPage((prev: any) => {
       return { ...prev, currentPage: currentPage };
@@ -68,35 +67,29 @@ const Dashboard = (props: Props) => {
     fetchData(currentPage || 0);
   };
 
-  // Hàm cập nhật trạng thái sản phẩm
   const toggleProductStatus = async (id: string, status: boolean) => {
     try {
-      // Kiểm tra trạng thái hiện tại của sản phẩm và gọi API thích hợp
       if (status) {
-        await DeactivateProduct(id); // Gọi API để vô hiệu hóa sản phẩm
+        await DeactivateProduct(id);
       } else {
-        await ActivateProduct(id); // Gọi API để kích hoạt sản phẩm
+        await ActivateProduct(id);
       }
 
-      // Cập nhật lại trạng thái của sản phẩm trong danh sách
       const updatedProducts = products.map((product) =>
         product._id === id
           ? {
-            ...product,
-            status: !status, // Đảo ngược trạng thái boolean: từ "active" sang "deactive" và ngược lại
-          }
+              ...product,
+              status: !status,
+            }
           : product
       );
 
-      setProduct(updatedProducts); // Cập nhật lại danh sách sản phẩm sau khi thay đổi trạng thái
+      setProduct(updatedProducts);
     } catch (error) {
       console.error("Error toggling product status:", error);
-      // Xử lý lỗi nếu có
     }
   };
 
-  // Lọc danh sách sản phẩm dựa trên tên sản phẩm
-  // Lọc sản phẩm theo giá
   const filterByPrice = (
     products: Iproduct[],
     minPrice: number,
@@ -107,7 +100,6 @@ const Dashboard = (props: Props) => {
     );
   };
 
-  // Lọc sản phẩm theo chất liệu
   const filterByMaterial = (
     products: Iproduct[],
     material: string
@@ -115,12 +107,11 @@ const Dashboard = (props: Props) => {
     return material === ""
       ? products
       : products.filter(
-        (product) =>
-          product.material?.name?.toLowerCase() === material.toLowerCase()
-      );
+          (product) =>
+            product.material?.name?.toLowerCase() === material.toLowerCase()
+        );
   };
 
-  // Lọc sản phẩm theo danh mục
   const filterByCategory = (
     products: Iproduct[],
     category: string
@@ -128,27 +119,22 @@ const Dashboard = (props: Props) => {
     return category === ""
       ? products
       : products.filter(
-        (product) =>
-          product.category?.name?.toLowerCase() === category.toLowerCase()
-      );
+          (product) =>
+            product.category?.name?.toLowerCase() === category.toLowerCase()
+        );
   };
   const getFilteredProducts = () => {
     let filtered = [...products];
 
-    // Lọc theo giá
     filtered = filterByPrice(filtered, filterPrice[0], filterPrice[1]);
 
-    // Lọc theo chất liệu
     filtered = filterByMaterial(filtered, filterMaterial);
 
-    // Lọc theo danh mục
     filtered = filterByCategory(filtered, selectedCategory);
-    // Lọc theo tên
     filtered = filterByName(filtered, filterName);
     return filtered;
   };
 
-  // Tạo một mảng mới chứa các danh mục duy nhất
   const uniqueCategories = Array.from(
     new Set(products.map((product) => product.category.name))
   );
@@ -168,42 +154,30 @@ const Dashboard = (props: Props) => {
     <>
       {loading && <LoadingComponent />}
       <NavLink to={"/admin/add"}>
-        <button className=" focus:outline-none text-white bg-sky-600 hover:bg-sky-900 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm text-clip overflow-hidden px-5 py-2.5 me-2 mb-2">
+        <button className="focus:outline-none text-white bg-sky-600 hover:bg-sky-900 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
           Thêm mới
         </button>
       </NavLink>
-      {/* Export button
-      <CSVLink
-        data={csvData}
-        filename={"products.csv"}
-        className="focus:outline-none text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-        target="_blank"
-      >
-        Xuất Chất liệu
-      </CSVLink> */}
 
-      {/* lọc sản phẩm theo danh mục */}
-      <div className="mb-4 flex gap-4 items-center">
-        {/* Lọc theo danh mục */}
+      <div className="mb-4 flex flex-col sm:flex-row gap-4 items-center">
         <select
           value={selectedCategory}
           onChange={(e) => setSelectedCategory(e.target.value)}
-          className="p-2 border border-gray-300 rounded"
+          className="p-2 border border-gray-300 rounded w-full sm:w-auto"
         >
-          <option value="">All Categories</option>
+          <option value="">Danh mục</option>
           {uniqueCategories.map((category) => (
             <option key={category} value={category}>
               {category}
             </option>
           ))}
         </select>
-        {/* Lọc theo chất liệu */}
         <select
           value={filterMaterial}
           onChange={(e) => setFilterMaterial(e.target.value)}
-          className="p-2 border border-gray-300 rounded"
+          className="p-2 border border-gray-300 rounded w-full sm:w-auto"
         >
-          <option value="">All Materials</option>
+          <option value="">Chất liệu</option>
           {Array.from(new Set(products.map((p) => p.material?.name))).map(
             (material) => (
               <option key={material} value={material}>
@@ -213,36 +187,50 @@ const Dashboard = (props: Props) => {
           )}
         </select>
 
-        {/* Lọc theo khoảng giá */}
-        <div className="flex gap-2">
+        <div className="flex flex-col sm:flex-row gap-2 w-full">
           <input
             type="number"
             placeholder="Min Price"
             value={filterPrice[0]}
             onChange={(e) => {
-              const value = Math.max(0, Number(e.target.value)); // Không cho phép giá trị âm
+              const value = Math.max(0, Number(e.target.value));
               setFilterPrice([value, filterPrice[1]]);
             }}
             onKeyDown={(e) => {
-              if (e.key === "-" || isNaN(Number(e.key))) e.preventDefault(); // Chặn nhập số âm và ký tự không hợp lệ
+              if (e.key === "-" || isNaN(Number(e.key))) e.preventDefault();
             }}
-            className="p-2 border border-gray-300 rounded w-[120px]"
+            className="p-2 border border-gray-300 rounded w-full sm:w-[120px]"
           />
+          <div className="mt-[5px]">
+            <svg
+              className="h-8 w-8 text-red-500"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4"
+              />
+            </svg>
+          </div>
           <input
             type="number"
             placeholder="Max Price"
             value={filterPrice[1]}
             onChange={(e) => {
-              const value = Math.max(0, Number(e.target.value)); // Không cho phép giá trị âm
+              const value = Math.max(0, Number(e.target.value));
               setFilterPrice([filterPrice[0], value]);
             }}
             onKeyDown={(e) => {
-              if (e.key === "-" || isNaN(Number(e.key))) e.preventDefault(); // Chặn nhập số âm và ký tự không hợp lệ
+              if (e.key === "-" || isNaN(Number(e.key))) e.preventDefault();
             }}
-            className="p-2 border border-gray-300 rounded w-[120px]"
+            className="p-2 border border-gray-300 rounded w-full sm:w-[120px]"
           />
         </div>
-        <div className="relative">
+        <div className="relative w-full">
           <input
             type="text"
             placeholder="Tìm kiếm theo tên sản phẩm"
@@ -250,75 +238,44 @@ const Dashboard = (props: Props) => {
             onChange={(e) => setFilterName(e.target.value)}
             className="p-2 border border-gray-300 rounded w-full"
           />
-          
         </div>
       </div>
 
       <div className="mb-[20px] flex flex-col w-full">
-        <div className="overflow-x-auto"> 
+        <div className="overflow-x-auto">
           <div className="py-2 inline-block w-full px-0">
             <div className="overflow-hidden">
               <table className="min-w-full table-auto">
                 <thead className="bg-white border-b">
                   <tr>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                    >
+                    <th className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                       Stt
                     </th>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                    >
+                    <th className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                       Tên Sản Phẩm
                     </th>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                    >
+                    <th className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                       Giá
                     </th>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                    >
+                    <th className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                       Danh mục
                     </th>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                    >
+                    <th className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                       Chất Liệu
                     </th>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                    >
+                    <th className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                       Số lượng
                     </th>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                    >
+                    <th className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                       Mô tả
                     </th>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                    >
+                    <th className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                       Ảnh
                     </th>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                    >
+                    <th className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                       Trạng Thái
                     </th>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                    >
+                    <th className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                       Handle
                     </th>
                   </tr>
@@ -327,33 +284,33 @@ const Dashboard = (props: Props) => {
                   {getFilteredProducts().map(
                     (product: Iproduct, index: number) => (
                       <tr className="bg-gray-100 border-b" key={product._id}>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                        <td className="px-6 py-4 text-sm font-medium text-gray-900">
                           {index + 1}
                         </td>
-                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <td className="px-6 py-4 text-sm font-light text-gray-900">
                           {product.name}
                         </td>
-                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <td className="px-6 py-4 text-sm font-light text-gray-900">
                           {new Intl.NumberFormat("vi-VN", {
                             style: "currency",
                             currency: "VND",
                           }).format(product.price)}
                         </td>
-                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <td className="px-6 py-4 text-sm font-light text-gray-900">
                           {product?.category?.name}
                         </td>
-                        <td className="px-6 py-4 text-sm truncate text-gray-900 max-w-xs">
+                        <td className="px-6 py-4 text-sm font-light text-gray-900 truncate max-w-xs">
                           {product?.material?.name}
                         </td>
-                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <td className="px-6 py-4 text-sm font-light text-gray-900">
                           {product.soLuong}
                         </td>
-                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <td className="px-6 py-4 text-sm font-light text-gray-900 truncate">
                           {product.moTa.length > 50
                             ? `${product.moTa.substring(0, 20)}...`
                             : product.moTa}
                         </td>
-                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <td className="px-6 py-4 text-sm font-light text-gray-900">
                           <img
                             className="w-[100px]"
                             src={product?.img[0]}
@@ -361,25 +318,19 @@ const Dashboard = (props: Props) => {
                           />
                         </td>
                         <td
-                          className={`text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap ${product.status === true
-                              ? "text-green-700"
-                              : "text-red-700"
-                            }`}
+                          className={`px-6 py-4 text-sm font-light text-gray-900 ${
+                            product.status ? "text-green-700" : "text-red-700"
+                          }`}
                         >
-                          {product.status === true
-                            ? "Hoạt động"
-                            : "Vô hiệu hóa"}
+                          {product.status ? "Hoạt động" : "Vô hiệu hóa"}
                         </td>
-                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <td className="px-6 py-4 text-sm font-light text-gray-900">
                           <button
                             onClick={() => updateProduct(product._id)}
-                            type="button"
                             className="focus:outline-none text-white bg-sky-600 hover:bg-sky-900 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
                           >
                             Edit
                           </button>
-
-                          {/* Nút Active/Deactive */}
                           <Popconfirm
                             title="Are you sure?"
                             onConfirm={() =>
@@ -389,13 +340,13 @@ const Dashboard = (props: Props) => {
                             cancelText="No"
                           >
                             <button
-                              type="button"
-                              className={`focus:outline-none text-white ${product.status === true
+                              className={`focus:outline-none text-white ${
+                                product.status
                                   ? "bg-red-700 hover:bg-red-800"
                                   : "bg-green-700 hover:bg-green-800"
-                                } focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2`}
+                              } focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2`}
                             >
-                              {product.status === true ? "Deactive" : "Active"}
+                              {product.status ? "Deactive" : "Active"}
                             </button>
                           </Popconfirm>
                         </td>
